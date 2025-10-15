7fe7543bd3c29d51ccb03d3fd40e0b77dd064361
Ilya <im486dx@gmail.com>
2025-10-15 23:48:55 +0400

    Calculator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package org.example
 
+import org.example.calculator.Calculator
 import org.example.calculator.Executor
 import org.example.calculator.Lexer
 import org.example.calculator.Parser
@@ -8,16 +9,14 @@ import org.example.calculator.Parser
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     try {
-        val lexer = Lexer()
-        val tokens = lexer.parse("-2 + 3 * -(x + 2) / 4.7")
-        val parser = Parser()
-        val root = parser.parse(tokens)
-        val executor = Executor(
+        val expression = "-2 + 3 * -(x + 2) / 4.7"
+        val calculator = Calculator(true)
+        val result = calculator.calculate(
+            expression,
             mapOf(
-                "x" to 1.0
-            ), true
+                "x" to 3.14
+            )
         )
-        val result = executor.execute(root)
         println("result: $result")
     } catch (e: Exception) {
         println(e.message)
